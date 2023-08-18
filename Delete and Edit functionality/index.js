f949ec265fda198ac664d32d648c126e4192f720
Bhuvneshja <bhuvneshjain2000@gmail.com>
2023-08-18 17:28:51 +0530

    Filter

--- Delete and Edit functionality/index.js
+++ Delete and Edit functionality/index.js
@@ -9,6 +9,7 @@ function addItem(e){
 
     // Get Input Value
     var newItem = document.getElementById('item').value;
+    var newDescription = document.getElementById('description').value;
 
     // Create New li Element
     var li = document.createElement('li');
@@ -17,7 +18,22 @@ function addItem(e){
     li.className = 'list-group-item';
 
     // Add Text Node With Input Value
-    li.appendChild(document.createTextNode(newItem));
+    // li.appendChild(document.createTextNode(newItem));
+    // Create and Append Item Name Span
+    var nameSpan = document.createElement('span');
+    nameSpan.className = 'item-name';
+    nameSpan.textContent = newItem;
+    li.appendChild(nameSpan);
+
+    // Add Description Below Item Name
+    // var smallElm = document.createElement('p');
+    // smallElm.appendChild(document.createTextNode('-'+newDescription));
+    // li.appendChild(smallElm);
+    // Create and Append Description Span
+    var desSpan = document.createElement('span');
+    desSpan.className = 'item-desc';
+    desSpan.textContent = ' ' + newDescription;
+    li.appendChild(desSpan);
 
     // Create Delete Button Element
     var delbtn = document.createElement('button');
@@ -45,4 +61,32 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// Filter Event
+var filter = document.getElementById('filter');
+filter.addEventListener('keyup',filterItem);
+
+function filterItem(e){
+    
+    // Convert Text To Lower Case
+    var text = e.target.value.toLowerCase();
+
+    // Get List
+    var items = itemList.getElementsByTagName('li');
+
+    // Convert To An Array
+    Array.from(items).forEach(function(item){
+        var itemNameElem = item.querySelector('.item-name');
+        var itemDescriptionElem = item.querySelector('.item-desc');
+
+        var itemName = itemNameElem ? itemNameElem.textContent : "";
+        var itemDescription = itemDescriptionElem ? itemDescriptionElem.textContent : "";
+        
+        if (itemName.toLowerCase().indexOf(text) != -1 || itemDescription.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    });
 }
