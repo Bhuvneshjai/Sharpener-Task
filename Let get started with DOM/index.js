a7c0775b6e61eaae8000fc49a2f990e44bbef33b
Bhuvneshja <bhuvneshjain2000@gmail.com>
2023-08-18 13:11:07 +0530

    QuerySelector && QuerySelectorALL

--- Let get started with DOM/index.js
+++ Let get started with DOM/index.js
@@ -56,8 +56,8 @@ submit.value = "SEND";
 var item = document.querySelector('.list-group-item');
 item.style.color = 'lightyellow';
 
-// var lastitem = document.querySelector('.list-group-item:last-child');
-// lastitem.style.color = 'red';
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.color = 'red';
 
 var seconditem = document.querySelector('.list-group-item:nth-child(3)');
 seconditem.style.color = 'brown';
@@ -70,18 +70,36 @@ var odd = document.querySelectorAll('li:nth-child(odd)');
 var even = document.querySelectorAll('li:nth-child(even)');
 for(var i=0;i<odd.length;i++){
     odd[i].style.backgroundColor = 'lightgrey';
-    // even[i].style.backgroundColor = 'white';
+    even[i].style.backgroundColor = 'white';
 }
 
 // Make the 3 rd element in the list have green background color
 // Make all the elements in the list have bold color font
 var thirdelement = document.getElementsByClassName('list-group-item');
-thirdelement[2].style.backgroundColor = 'lightgreen';
+thirdelement[2].style.backgroundColor = 'red';
 for(var i=0;i<thirdelement.length;i++){
     thirdelement[i].style.fontWeight = 'bold';
 }
 
 // And try editing it with getelementsbyclassname and then by getelementbytagname
 // If we edit particular element by class name it does not effect the new item without class name
-var tagname = document.getElementsByTagName('li');
-tagname[4].style.color = 'yellow';
+// var tagname = document.getElementsByTagName('li');
+// tagname[4].style.color = 'yellow';
+
+// Make the 2nd item have green background color
+var seconditemgreenbg = document.querySelector('.list-group-item:nth-child(2)');
+seconditemgreenbg.style.backgroundColor = 'lightgreen';
+
+// Make the 3rd item invisible
+var thirditeminvisible = document.querySelector('.list-group-item:nth-child(3)');
+thirditeminvisible.style.display = 'none';
+
+// Using QuerySelectorALL change the font color to green for 2nd item in the item list
+var seconditemgreencolor = document.querySelectorAll('.list-group-item');
+seconditemgreencolor[1].style.color = 'green'
+
+// Choose all the odd elements and make their background green using QuerySelectorALL﻿
+var oddelementgreen = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(var i=0;i<oddelementgreen.length;i++){
+    oddelementgreen[i].style.backgroundColor = 'lightgreen';
+}
